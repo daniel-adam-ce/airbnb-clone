9d4413b5089bd448a62e3e17706c79d800e28ed9
Daniel Adam <danieladamce@gmail.com>
2025-01-17 20:01:11 -0800

    refactoring config module and add jwt/port env vars

--- apps/reservations/src/main.ts
+++ apps/reservations/src/main.ts
@@ -1,7 +1,8 @@
 import { NestFactory } from '@nestjs/core';
 import { ReservationsModule } from './reservations.module';
 import { ValidationPipe } from '@nestjs/common';
-import { Logger } from 'nestjs-pino'; 
+import { Logger } from 'nestjs-pino';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(ReservationsModule);
@@ -9,6 +10,8 @@ async function bootstrap() {
     whitelist: true
   }));
   app.useLogger(app.get(Logger))
-  await app.listen(process.env.port ?? 3000);
+  app.useLogger(app.get(Logger))
+  const configService = app.get(ConfigService);
+  await app.listen(configService.get("PORT"));
 }
 bootstrap();
