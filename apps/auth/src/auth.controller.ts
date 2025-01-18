edb8196ef9ab69d82ffc86bc1191b65f5457fc0f
Daniel Adam <danieladamce@gmail.com>
2025-01-17 21:02:23 -0800

    local strategy and login logic

--- apps/auth/src/auth.controller.ts
+++ apps/auth/src/auth.controller.ts
@@ -1,9 +1,23 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Post, Res, UseGuards } from '@nestjs/common';
 import { AuthService } from './auth.service';
+import { LocalAuthGuard } from './users/guards/local-auth.guard';
+import { CurrentUser } from './users/current-user.decorator';
+import { UserDocument } from './users/schema/users.schema';
+import { Response } from 'express';
 
 @Controller()
 export class AuthController {
-  constructor(private readonly authService: AuthService) {}
+  constructor(private readonly authService: AuthService) { }
+
+  @UseGuards(LocalAuthGuard)
+  @Post("/login")
+  async login(
+    @CurrentUser() user: UserDocument,
+    @Res({ passthrough: true }) response: Response
+  ) {
+    await this.authService.login(user, response);
+    response.send(user)
+  }
 
   @Get()
   getHello(): string {
