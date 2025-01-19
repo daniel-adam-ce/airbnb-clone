4604eda6c224193ed5f8456cdb2aa8005ebbf576
Daniel Adam <danieladamce@gmail.com>
2025-01-19 13:22:24 -0800

    stripe setup

--- apps/payments/src/payments.service.ts
+++ apps/payments/src/payments.service.ts
@@ -1,7 +1,18 @@
 import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import Stripe from 'stripe';
 
 @Injectable()
 export class PaymentsService {
+
+  private readonly = new Stripe(this.configSerivce.get("STRIPE_SECRET_KEY"),
+    {
+      apiVersion: "2024-12-18.acacia"
+    }
+  )
+
+  constructor(private readonly configSerivce: ConfigService) { }
+
   getHello(): string {
     return 'Hello World!';
   }
