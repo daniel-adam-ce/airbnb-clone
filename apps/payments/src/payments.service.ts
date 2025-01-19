eed63d8cb3fc4aa171b5520300757765d988fd28
Daniel Adam <danieladamce@gmail.com>
2025-01-19 13:29:34 -0800

    createCharge method

--- apps/payments/src/payments.service.ts
+++ apps/payments/src/payments.service.ts
@@ -5,7 +5,7 @@ import Stripe from 'stripe';
 @Injectable()
 export class PaymentsService {
 
-  private readonly = new Stripe(this.configSerivce.get("STRIPE_SECRET_KEY"),
+  private readonly stripe = new Stripe(this.configSerivce.get("STRIPE_SECRET_KEY"),
     {
       apiVersion: "2024-12-18.acacia"
     }
@@ -13,7 +13,23 @@ export class PaymentsService {
 
   constructor(private readonly configSerivce: ConfigService) { }
 
-  getHello(): string {
-    return 'Hello World!';
+  async createCharge(
+    card: Stripe.PaymentMethodCreateParams.Card,
+    amount: number
+  ) {
+    const paymentMethod = await this.stripe.paymentMethods.create({
+      type: "card",
+      card: card
+    })
+
+    const paymentIntent = await this.stripe.paymentIntents.create({
+      payment_method: paymentMethod.id,
+      amount: amount * 100,
+      confirm: true,
+      payment_method_types: ["card"],
+      currency: "usd"
+    })
+
+    return paymentIntent;
   }
 }
