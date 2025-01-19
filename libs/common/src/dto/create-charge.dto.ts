6863ca888233bfaf6bc553ea92e8db14f7df5df0
Daniel Adam <danieladamce@gmail.com>
2025-01-19 13:42:57 -0800

    abstracted card and charge dtos into common lib

---
+++ libs/common/src/dto/create-charge.dto.ts
@@ -0,0 +1,12 @@
+import { CardDto } from "./card.dto";
+import { IsDefined, IsNotEmpty, IsNumber, ValidateNested } from "class-validator";
+
+export class CreateChargeDto {
+    @IsDefined()
+    @IsNotEmpty()
+    @ValidateNested()
+    card: CardDto;
+
+    @IsNumber()
+    amount: number;
+}
